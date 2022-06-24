226bc60b0f98b45d8c83ff8bee564a89cae842ef
MARY <marymwihaki123@gmail.com>
2022-06-24 16:57:48 -0500

    solving last problem

--- js/app.js
+++ js/app.js
@@ -116,13 +116,17 @@ function sumArray(sumarray) { //eslint-disable-line
 
 // Here is the test for sumArray(); uncomment it to run it
 
-testSumArray(testArray);
+//testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 5
-Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called multiplyArray() that takes an array
+ of numbers as its argument and returns an array whose first element
+  is the product of those numbers, and the second element
+   is a string that EXACTLY follows this example and uses
+    the values that were input into the function:
 
 "The numbers 2,3,4 have a product of 24."
 
@@ -131,14 +135,24 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-
+//'The numbers 2,3,4 have a product of 24.'
+//'The numbers 3,4 have a product of 12.'
 function multiplyArray(multArr) { //eslint-disable-line
-  let total=1 ;
-
+  let theProduct = 1;
+  let numberAString='';
+  let punctuation ='';
+  for(let i = 0;i < multArr.length; i++) {
+    theProduct *= multArr [i];
+    numberAString += punctuation + multArr[i];
+    punctuation= ',';
+  } 
+  return [theProduct,`The numbers ${numberAString} have a product of ${theProduct}.`];
 }
+  
+
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
@@ -162,7 +176,7 @@ Test this function by hand in the console to get it working, and when you think 
 let testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+    
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
